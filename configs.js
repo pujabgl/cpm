6dbd9ea8d6d324280a190752060cc15fab6a9f0f
pujabgl <40192032+pujabgl@users.noreply.github.com>
2018-06-20 15:46:05 +0530

    Add files via upload

---
+++ configs.js
@@ -0,0 +1,15 @@
+//configs.js
+// Rename this file to configs.js 
+// Define your Application Configurations here
+var config = {
+    // Meraki API Key
+    apiKey: "b5bbbdd45c9b480d0da6056ea25763d885ecc8f4", // Demo API Key
+    // Meraki API Base URL
+    apiUrl: "https://api.meraki.com/api/v0", 
+    // The Meraki Network ID
+    networkId: "L_669347494617946620", 
+    // The Meraki Group Policy ID 
+    policy: "100"
+}
+
+module.exports = config;
